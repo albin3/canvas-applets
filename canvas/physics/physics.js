a4752fd5bd80fc3016d42f38a398ded1db29972c
马肱 <binwei.zbw@alibaba-inc.com>
2015-09-29 10:07:33 +0800

    匀速直线运动

--- canvas/physics/physics.js
+++ canvas/physics/physics.js
@@ -20,31 +20,71 @@ $(document).ready(function() {
     playAnimation = false;
   });
 
-  var v = 5;        //速度
+  //全局变量
   var margin = 100; //margin
-  var x = margin;   //方块在屏幕中的x位置
   var w = 20;       //方块的大小
+
+  var baseLine = (function () {
+    return function () {
+      //左边的线
+      context.beginPath();
+      context.moveTo(margin, 0);
+      context.lineTo(margin, canvasHeight);
+      context.stroke();
+     
+      //右边的线
+      context.beginPath();
+      context.moveTo(canvasWidth - margin, 0);
+      context.lineTo(canvasWidth - margin, canvasHeight);
+      context.stroke();
+    };
+  })();
+
+  //匀速直线运动
+  var uniformMotion = (function () {
+    var v = 5;        //速度
+    var x = margin;   //方块在屏幕中的x位置
+
+    return function() {
+      context.fillStyle = '#5CA0E7';
+      context.fillRect(x, 50, w, w);
+      x = x + v;
+      if (x + w >= canvasWidth - margin || x <= margin) {
+        v = -v;
+      }
+    };
+  })();
+
+  //匀加速运动
+  var uniformlyAcceleratedMotion = (function () {
+    var v = 0;
+    var a = 1;
+    var x = margin;
+
+    return function () {
+      context.fillStyle = '#5CA0E7';
+      context.fillRect(x, 100, w, w);
+      v = v + a;
+      if (x + v >= canvasWidth - margin) {
+        v = -v;
+      } else {
+        x = x + v;
+      }
+    };
+  })();
+
+
   function animate() {
     context.clearRect(0, 0, canvasWidth, canvasHeight);
 
-    //左边的线
-    context.beginPath();
-    context.moveTo(margin, 0);
-    context.lineTo(margin, canvasHeight);
-    context.stroke();
+    //基准线
+    baseLine();
 
-    //右边的线
-    context.beginPath();
-    context.moveTo(canvasWidth - margin, 0);
-    context.lineTo(canvasWidth - margin, canvasHeight);
-    context.stroke();
+    //匀速直线运动
+    uniformMotion();
 
-    context.fillStyle = '#5CA0E7';
-    context.fillRect(x, 200, w, w);
-    x = x + v;
-    if (x + w >= canvasWidth - margin || x <= margin) {
-      v = -v;
-    }
+    //匀加减速直线运动
+    uniformlyAcceleratedMotion();
 
     if (playAnimation) {
       setTimeout(animate, 33);
